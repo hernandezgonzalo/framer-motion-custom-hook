878fb71b41b853adf5b7bdd1dd94c08f6a634360
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-07-11 20:13:27 +0200

    project configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./index.css";
+import { motion } from "framer-motion";
 
-function App() {
+export default function App() {
+  const outerRef = useRef(null);
+  const [inViewport, setInViewport] = useState(true);
+
+  useEffect(() => {
+    const onChange = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.target === outerRef.current) {
+          if (entry.isIntersecting) {
+            setInViewport(true);
+          } else {
+            setInViewport(false);
+          }
+        }
+      });
+    };
+    const observer = new IntersectionObserver(onChange, { threshold: 0.5 });
+    observer.observe(outerRef.current);
+  }, [outerRef]);
+
+  // Framer motion animations
+  const fadeInContainerWithStagger = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.25,
+        type: "tween",
+        ease: "easeIn",
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const fadeInUp = {
+    hidden: {
+      opacity: 0,
+      y: 40,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+      },
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="previous-content">Scroll down </div>
+      <div ref={outerRef} id="outer-box">
+        {inViewport && (
+          <motion.div
+            variants={fadeInContainerWithStagger}
+            initial="hidden"
+            animate="visible"
+          >
+            <motion.div variants={fadeInUp}>
+              <h2>Una zoonosis emergente</h2>
+            </motion.div>
+
+            <motion.div variants={fadeInUp}>
+              Una zoonosis es una enfermedad infecciosa transmitida de un
+              animal...
+            </motion.div>
+          </motion.div>
+        )}
+      </div>
+    </>
   );
 }
-
-export default App;
